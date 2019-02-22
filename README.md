706ddf1c57da59f1bffb94ad65c054027bc802b1
Max <max3jk@gmail.com>
2019-02-22 17:45:18 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Windows-Python-environment-variable-switcher
 Windows Python (3 - 2) environment variable switcher PowerShell
+
+Run:
+1. Add the path to the python folder to the beginning of the "py2enable.ps1" and "py3enable.ps1" files
+2. Run one of the scripts using powershell (right mouse button - run using powershell)
---
+++ py2enable.ps1
@@ -0,0 +1,49 @@
+$python2Path = 'C:\Python27'
+$python3Path = 'C:\Program Files (x86)\Python36-32'
+
+
+
+# param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+
+exit
+}
+
+'running with full privileges'
+
+
+
+
+# Get it
+$path = [System.Environment]::GetEnvironmentVariable(
+    'PATH',
+    'Machine'
+)
+# Remove unwanted elements
+$path = ($path.Split(';') | Where-Object { $_ -ne $python3Path }) -join ';'
+$path = ($path.Split(';') | Where-Object { $_ -ne ($python3Path+'\Scripts') }) -join ';'
+$path = ($path.Split(';') | Where-Object { $_ -ne $python2Path }) -join ';'
+$path = ($path.Split(';') | Where-Object { $_ -ne ($python2Path+'\Scripts') }) -join ';'
+$path += ";"+$python2Path
+$path += ";"+$python2Path+'\Scripts'
+# Set it
+[System.Environment]::SetEnvironmentVariable(
+    'PATH',
+    $path,
+    'Machine'
+)
+
+'python 2 enabled'
